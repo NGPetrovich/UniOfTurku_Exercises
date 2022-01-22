aef35e4e889dff91b5ceb01bc036f1d80fc7c362
Nikita Goncharenko <nikita.122495@gmail.com>
2022-01-22 22:03:57 +0100

    added delete and post methods

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 let notes = [
     {
@@ -22,13 +25,66 @@ let notes = [
     }
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+app.get('/notes', (request, response) => {
+    response.send(notes)
 });
 
-app.get('/notes', (req, res) => {
-    res.json(notes)
+app.get('/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const note = notes.find(note => note.id === id)
+    // const note = notes.find(note => {
+    //     console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+    //     return note.id === id
+    //     //parseInt(id)
+    // })
+
+    if ( note ) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+    // console.log(note)
+    // response.json(note)
 });
+
+app.delete('/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? notes.map(n => n.id).sort((a,b) => a - b).reverse()[0] : 1
+  return maxId + 1
+}
+
+app.post('/notes', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important|| false,
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+// app.post('/notes', (request, response) => {
+//   const note = request.body
+//   console.log(note)
+
+//   response.json(note)
+// })
 
 const PORT = 3001;
 app.listen(PORT, () => {
