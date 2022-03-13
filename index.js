030f630da0995f853e3688072651f183c99c5cb9
Nikita Goncharenko <nikita.122495@gmail.com>
2022-03-13 14:05:48 +0100

    connected GET and POST with frontend to database

--- index.js
+++ index.js
@@ -3,16 +3,58 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const Person = require('./models/person');
+const { request } = require('express');
+const { response } = require('express');
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(bodyParser.json())
 
+const formatPerson = (person) => {
+  const formattedPerson = { ...person._doc, id: person._id }
+  delete formattedPerson._id
+  delete formattedPerson.__v
+
+  return formattedPerson
+}
+
 app.get('/api/persons', (request, response) => {
   Person
     .find({})
     .then(persons => {
-      response.json(persons)
+      response.json(persons.map(formatPerson))
+      console.log(persons.map(formatPerson))
+    })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      response.json(formatPerson(person))
+    })
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (body.name === undefined) {
+    return response.status(400).json({error: 'name is missing'})
+  }
+  
+  if (body.phone === undefined) {
+    return response.status(400).json({error: 'number is missing'})
+  }
+
+  const person = new Person({
+    name: body.name,
+    phone: body.phone
+  })
+
+  person.
+    save()
+    .then(savedPerson => {
+      response.json(formatPerson(savedPerson))
     })
 })
 
--- models/person.js
+++ models/person.js
@@ -10,35 +10,4 @@ const Person = mongoose.model('Person', {
   id: Number
 })
 
-// const { argv } = process;
-// const [, , inputName, inputPhone, inputID] = argv;
-
-// const person = new Person({
-//   name: inputName,
-//   phone: inputPhone,
-//   id: inputID
-// })
-
-// // console.log(contact.name)
-// // console.log(contact.number)
-
-// if (!person.name && !person.phone) {
-//   Person
-//     .find({})
-//     .then(persons => {
-//       console.log("Contacts:")
-//       persons.forEach(person => {
-//         console.log(person.name, person.phone)
-//       })
-//       mongoose.connection.close()
-//     })
-// } else {
-//   person
-//     .save()
-//     .then(result => {
-//       console.log(`adding person ${person.name} number ${person.phone} to the directory`);
-//       mongoose.connection.close()
-//     })
-// }
-
 module.exports = Person
